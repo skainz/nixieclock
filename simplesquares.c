f61a12feaeee43c3a8a695ffe8d6e73d9f4df84f
Simon Kainz <simon@familiekainz.at>
2019-05-07 23:32:27 +0200

    initialize array of all digit images

--- simplesquares.c
+++ simplesquares.c
@@ -5,6 +5,28 @@
 
 
 #include "images/WikipediaNixieDigit0.xpm"
+#include "images/WikipediaNixieDigit1.xpm"
+#include "images/WikipediaNixieDigit2.xpm"
+#include "images/WikipediaNixieDigit3.xpm"
+#include "images/WikipediaNixieDigit4.xpm"
+#include "images/WikipediaNixieDigit5.xpm"
+#include "images/WikipediaNixieDigit6.xpm"
+#include "images/WikipediaNixieDigit7.xpm"
+#include "images/WikipediaNixieDigit8.xpm"
+#include "images/WikipediaNixieDigit9.xpm"
+
+
+static char **digits[]={WikipediaNixieDigit0,
+			WikipediaNixieDigit1,
+			WikipediaNixieDigit2,
+			WikipediaNixieDigit3,
+			WikipediaNixieDigit4,
+			WikipediaNixieDigit5,
+			WikipediaNixieDigit6,
+			WikipediaNixieDigit7,
+			WikipediaNixieDigit8,
+			WikipediaNixieDigit9
+};
 main ()
 {
   Display *dpy;
@@ -29,7 +51,7 @@ main ()
   XSetForeground(dpy, g, WhitePixelOfScreen(DefaultScreenOfDisplay(dpy)) );
 
 
- if (XpmCreateImageFromData  (dpy, WikipediaNixieDigit0, &img, NULL, NULL)) {
+ if (XpmCreateImageFromData  (dpy, digits[2], &img, NULL, NULL)) {
 //      printf ("Error reading image\n");
       exit (1);
 }
@@ -40,7 +62,7 @@ main ()
       /* draw a square */
       XFillRectangle (dpy, root, g, random()%500, random()%500, 50, 40);
 
-   XPutImage(dpy,root,g,img,0,0,0,0,200,200);
+   XPutImage(dpy,root,g,img,0,0,random()%500,random()%500,200,200);
 
       /* once in a while, clear all */
       if( random()%500<1 )
